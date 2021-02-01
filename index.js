9da0f8da83bc7f279dd76d34a7572b05aabe4218
Alex Marz <alexhmarz@gmail.com>
2021-02-01 16:22:46 -0500

    commit tree 06bbdf8b82976767d59d92307a865306e3399c23

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // ***** Question 1 *****
 // *** Uncomment the lines below to test
-// console.log("%cQuestion 1", "color: red") 
+//console.log("%cQuestion 1", "color: red") 
+let counter = 1
 
-// console.log(counter) 
+counter = 2 
+//console.log(counter) 
 // // => 2
 // console.log("%c----------", "color: red") 
 
